85df4c5dcf4cdbdc26c0485d7efdc52a9d1d43d5
Kyrylo Dyachenko <mallet25460@gmail.com>
2023-03-07 11:34:21 +0200

    try

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = 'mongodb+srv://mallet25460:fXxAFmT8nkuDzPnp@cluster0.499jj56.mongodb.net/db-contacts?retryWrites=true&w=majority'
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_HOST)
+.then (() => app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+  console.log("Database connection successful")
+}))
+.catch (error => {
+  console.log(error.messege);
+process.exit(1);})
+
+
